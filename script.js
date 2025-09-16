64a96614e4d4c3ffc0c8d5684b5eed445ecf1085
Sandra Hagevall <sandrahagevall@hotmail.com>
2025-09-16 16:06:08 +0200

    add no scroll when hamburger menu is open

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger")
 const navLinks = document.querySelector(".nav-links")
+const body = document.body;
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active")
   hamburger.classList.toggle("open")
+  body.classList.toggle("no-scroll")
 })
